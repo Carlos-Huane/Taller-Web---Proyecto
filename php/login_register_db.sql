dc457f1434e4f93f9b399f90df515359adaaef0b
Ekzzzzzz <mariadelosangeles505@hotmail.com>
2024-12-05 21:47:54 -0500

    bd
    bd

--- php/login_register_db.sql
+++ php/login_register_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-11-2024 a las 11:13:13
+-- Tiempo de generación: 06-12-2024 a las 03:47:13
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
